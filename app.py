1e0baf58193a05c388a229e1e3fb7efcea5d1de5
CECILIAM14 <162057872+CECILIAM14@users.noreply.github.com>
2024-03-11 17:20:25 -0600

    no headers

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ import streamlit as st
 
 st.header('Lanzar una moneda')
 
-st.write('Esta aplicación aún no es funcional. En construcción.')
-
-st.header('Lanzar una moneda')
-
 number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
 start_button = st.button('Ejecutar')
 
